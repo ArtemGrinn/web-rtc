9a69eb0540a5c59476e01fcc1a99c731c252f5b7
Art Grinn <grinn.art@gmail.com>
2021-03-09 17:11:22 +0300

    fix static

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ const fs = require("fs");
 const port = process.env.PORT || 3000;
 const wss = new WebSocket.Server({ port: port });
 console.log("Listening port: " + port);
-wss.on('connection', (ws, req) => {
+
+
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+wss.on('connection', (ws) => {
 	console.log('connect');
-  	const fileStream = fs.createWriteStream('./output', { flags: 'a' });
+  	const fileStream = fs.createWriteStream('./output.mp4', { flags: 'a' });
  	 ws.on('message', message => {
 	  	console.log('recieving');
-	    // Only raw blob data can be sent
 	    fileStream.write(Buffer.from(new Uint8Array(message)));
   	});
+	ws.on("close", function() {
+		console.log("websocket connection close")
+		clearInterval(id)
+	});
 });
